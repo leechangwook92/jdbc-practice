d8d991db347493b9971c2b1649a3274dff2b5fe2
changwook lee <lcy12225@naver.com>
2023-04-25 03:01:28 +0900

    before improving jdbc practice

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.Objects;
+
+public class User {
+    private final String userId;
+    private final String name;
+    private final String email;
+    private final String password;
+
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUerId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(getUerId(), user.getUerId()) && Objects.equals(getName(), user.getName()) && Objects.equals(getEmail(), user.getEmail()) && Objects.equals(getPassword(), user.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUerId(), getName(), getEmail(), getPassword());
+    }
+
+
+}
---
+++ src/test/java/UserDaoTest.java
@@ -0,0 +1,32 @@
+import org.example.ConnectionManager;
+import org.example.User;
+import org.example.UserDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserDaoTest {
+
+    @BeforeEach  // Test코드 실행 이전에 수행해야할 작업이 필요할때 만듬
+    void setUp() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("db_schema.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+
+    @Test
+    void createTest() throws SQLException {
+        UserDao userDao = new UserDao();
+
+        userDao.create(new User("wizard", "password", "name", "email"));
+
+        User user = userDao.findByUserId("wizard");
+        assertThat(user).isEqualTo(new User("wizard", "password", "name", "email"));
+    }
+}
